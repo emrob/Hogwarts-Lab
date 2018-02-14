b907d415640f33f0606949879dd2dc473fc39ae9
Emma Roberts <emrsixtyseven@gmail.com>
2018-02-14 16:31:27 +0000

    house table and save added

--- db/hogwarts.sql
+++ db/hogwarts.sql
@@ -1,4 +1,6 @@
+DROP TABLE house;
 DROP TABLE students;
+
 
 CREATE TABLE students (
 id serial4 PRIMARY KEY,
@@ -6,5 +8,12 @@ first_name VARCHAR(255),
 second_name VARCHAR(255),
 house VARCHAR(255),
 age INT
+
+);
+
+CREATE TABLE house (
+  id serial4 PRIMARY KEY,
+  name VARCHAR(255)
+
 
 );
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require("pry")
 require_relative("../models/student")
+require_relative("../models/house")
 
 Student.delete_all()
 
@@ -22,6 +23,26 @@ student1 = Student.new ({
 student1.save()
 student2.save()
 
+house1 = House.new ({
+"name" => "Gryffindor"
+})
+
+house2 = House.new ({
+"name" => "Hufflepuff"
+})
+
+house3 = House.new ({
+"name" => "Slytherin"
+})
+
+house4 = House.new ({
+  "name" => "Ravenclaw"
+  })
+
+house1.save()
+house2.save()
+house3.save()
+house4.save()
 
 binding.pry
 nil
